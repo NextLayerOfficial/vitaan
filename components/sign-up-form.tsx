a500496ca93d5d189ae27b9c7ac093929bcb2130
Yash <yashkd12790@gmail.com>
2025-12-02 14:20:53 +0530

    refactor: update authentication middleware to allow sign-up and enforce admin approval on sign-in

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -52,7 +52,7 @@ export default function SignUpForm() {
         currentCompany: "",
         jobTitle: "",
         phone: "",
-        imageKey: ""
+        imageKey: "",
       },
       {
         onRequest: () => {
